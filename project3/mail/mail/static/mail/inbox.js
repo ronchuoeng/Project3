e14c9b3f8f8e9856a49d7bc04cb81025610c98b1
ronchuoeng <jwei_eng@hotmail.com>
2022-12-13 13:51:38 +0800

    archive finally done

--- project3/mail/mail/static/mail/inbox.js
+++ project3/mail/mail/static/mail/inbox.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email); 
-
   
   // Compose 
   document.querySelector('#compose-form').addEventListener('submit', send_email);
@@ -37,6 +36,7 @@ function load_mailbox(mailbox) {
   document.querySelector('#container').style.display = 'none';
 
 
+
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
@@ -44,58 +44,65 @@ function load_mailbox(mailbox) {
       console.log(emails);
 
       // Loops for mails.
-      emails.forEach(contents => {
+      emails.forEach((contents) => {
         // Create the Div for each mail
         // Parent Div
         const mail = document.createElement('div');
         // Child Div
-        const mailSender = document.createElement('div');
+        const mailHeader = document.createElement('div');
         const mailSubject = document.createElement('div');
         const mailTime = document.createElement('div');
         
         // Create anchor tag for individual mail
         const mailTag = document.createElement('a');
-        mailTag.setAttribute('onclick', "return view_email('" + contents.id + "')");
+        
+        mailTag.setAttribute('data-code', contents.id);
+        mailTag.onclick = function(e) {
+          e.preventDefault();
+          view_email(this.getAttribute("data-code"));
+        }
 
         // Name the Parent Div
         mail.className = 'mailbox';
         // Name the Child Div
-        mailSender.className = 'mailbox_sender';
+        mailHeader.className = 'mailbox_header';
         mailSubject.className = 'mailbox_subject';
         mailTime.className = 'mailbox_time';
-
         
-
         // Add the contents inside Child Div
-        mailSender.innerHTML = contents.sender;
+        // Difference of the header between Sent & other mailboxes
+        if (mailbox == 'sent') {
+          mailHeader.innerHTML = contents.recipients;
+        } else {
+          mailHeader.innerHTML = contents.sender;
+        };
+
         mailSubject.innerHTML = contents.subject;
         mailTime.innerHTML = contents.timestamp;
-        // Set link for individual mail
-        mailTag.href = "#";
+        
 
-
-        // fill the div with contents into each mail
-        mail.appendChild(mailSender);
+        // add the Child div into the mail div
+        mail.appendChild(mailHeader);
         mail.appendChild(mailSubject);
         mail.appendChild(mailTime);
-        // add the done mail into anchor tag area
+        // add the done mail div into anchor tag area
         mailTag.appendChild(mail);
 
-        // If the mail is unread, turn background-color to grey, else white.
+        // If the mail is unread, turn background-color to grey, else white
         if (contents.read === true) {
           mail.style.backgroundColor = 'white';
         } else {
           mail.style.backgroundColor = 'rgba(128, 128, 128, 0.14)';
         }
-
+        
         // add the done-looping emails into the emails-views for ultimately show
         document.querySelector('#emails-view').append(mailTag);
       });
-
-
+      
+      
   });
+  
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-
 }
 
 
@@ -115,7 +122,7 @@ function send_email(e) {
       })
     })
     .then((response) => {
-      console.log(response)
+      console.log(response);
       return response.json();
     })
     .then(result => {
@@ -136,7 +143,6 @@ function view_email(mail_id){
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#container').style.display = 'block';
 
-  // Clear out composition fields
 
   fetch(`/emails/${mail_id}`)
   .then(response => response.json())
@@ -145,22 +151,68 @@ function view_email(mail_id){
       console.log(email);
 
       // ... do something else with email ...
+      
       document.querySelector('#container-sender').innerHTML = email.sender;
       document.querySelector('#container-recipients').innerHTML = email.recipients;
       document.querySelector('#container-subject').innerHTML = email.subject;
       document.querySelector('#container-body').innerHTML= email.body;
       document.querySelector('#container-timestamp').innerHTML = email.timestamp;
 
-      read_email(mail_id);
+      // function READ
+      if(!email.read) {
+        read_email(mail_id);
+      };
+
+      // Show the button of Archive or Unarchive.
+      user = document.querySelector('h2').innerHTML;
+      if (user == email.sender) {
+        document.querySelector('#archive').style.display = 'none';
+        document.querySelector('#unarchive').style.display = 'none';
+      } else if (!email.archived) {
+        document.querySelector('#archive').style.display = 'block';
+        document.querySelector('#unarchive').style.display = 'none';
+      } else if (email.archived) {
+        document.querySelector('#archive').style.display = 'none';
+        document.querySelector('#unarchive').style.display = 'block';
+      }
+
+      // Define the function for the button on click.
+      document.querySelector('#archive').onclick = () => archive(mail_id);
+      document.querySelector('#unarchive').onclick = () => unarchive(mail_id);
   });
 }
 
 function read_email(mail_id) {
 
+  // Turn the status of email to Read
   fetch(`/emails/${mail_id}`, {
     method: 'PUT',
     body: JSON.stringify({
         read: true
     })
   })
+}
+
+function archive(mail_id) {
+
+  fetch(`/emails/${mail_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: true
+    })
+  })
+  
+  load_mailbox('inbox');
+};
+
+function unarchive(mail_id) {
+
+  fetch(`/emails/${mail_id}`,{
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: false
+    })
+  })
+
+  load_mailbox('inbox');
 }
